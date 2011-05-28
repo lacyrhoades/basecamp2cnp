38838b6e443e91252a3304447b8ac1f95f5b95dd
Lacy Rhoades <lacyrhoades@gmail.com>
2011-05-27 22:35:08 -0500

    [config] registering vendor and 5.3 classes in project bootstrapping step

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -9,6 +9,36 @@ class ProjectConfiguration extends sfProjectConfiguration
   {
     $this->enablePlugins('sfDoctrinePlugin');
     
+    $this->registerBasecampApi();
+    $this->registerZend();
+    $this->registerBasecamp2Cnp();
+    
     require_once dirname(__FILE__).'/custom.php';
+  }
+  
+  private function registerBasecampApi()
+  {
+    require_once sfConfig::get('sf_lib_dir').'/vendor/Sirprize/Basecamp.php';
+  }
+  
+  private function registerZend()
+  {
+    $lib = sfConfig::get('sf_lib_dir').'/vendor';
+
+    $path = array(
+      $lib,
+      get_include_path()
+    );
+
+    set_include_path(implode(PATH_SEPARATOR, $path));
+
+    require_once sfConfig::get('sf_lib_dir').'/vendor/Zend/Loader/Autoloader.php';
+  }
+  
+  private function registerBasecamp2Cnp()
+  {
+    require_once sfConfig::get('sf_lib_dir').'/basecamp2cnp/Project/Manager.php';
+    require_once sfConfig::get('sf_lib_dir').'/basecamp2cnp/People/Manager.php';
+    require_once sfConfig::get('sf_lib_dir').'/basecamp2cnp/TimeEntry/Manager.php';
   }
 }
