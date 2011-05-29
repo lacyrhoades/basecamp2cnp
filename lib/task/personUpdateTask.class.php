380d1814570e1771b1914339463ad8a4a33af0ae
Lacy Rhoades <lacyrhoades@gmail.com>
2011-05-28 22:06:14 -0500

    [task] added tasks for data massaging

---
+++ lib/task/personUpdateTask.class.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * task for updating the person table from the Basecamp API
+ */
+class personUpdateTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    // // add your own arguments here
+    // $this->addArguments(array(
+    //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
+    // ));
+
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'doctrine'),
+      // add your own options here
+    ));
+
+    $this->namespace        = 'cron';
+    $this->name             = 'person-update';
+    $this->briefDescription = '';
+    $this->detailedDescription = <<<EOF
+The [person-update|INFO] task calls basecamp and asks for all the "person"
+employed by our company. It then updates the local database to relflect what
+it finds.
+
+Call it with:
+
+  [php symfony cron:person-update|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    // initialize the database connection
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+    // setup the basecamp connection
+    $config = sfConfig::get('basecamp_config', array());
+    $basecampApi = new \Sirprize\Basecamp($config);
+
+
+    // grab the person managing service
+    $personManager = new \basecamp2cnp\Person\Manager($basecampApi);
+
+    // set the task so we can have logging feedback to stdout
+    $personManager->setTask($this);
+
+    // grab the person table so we can add things to the database
+    $personTable = Doctrine::getTable('person');
+
+    // call the update person method, passing the person table.
+    $personManager->updatePerson($personTable);
+  }
+}
---
+++ lib/task/timeUpdateTask.class.php
@@ -0,0 +1,66 @@
+<?php
+
+class timeUpdateTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    // // add your own arguments here
+    // $this->addArguments(array(
+    //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
+    // ));
+
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'doctrine'),
+      // add your own options here
+    ));
+
+    $this->namespace        = 'cron';
+    $this->name             = 'time-update';
+    $this->briefDescription = '';
+    $this->detailedDescription = <<<EOF
+The [time-update|INFO] task pulls "time entries" from the Basecamp API and
+puts them in the local database.
+Call it with:
+
+  [php symfony cron:time-update|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    // initialize the database connection
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+    // setup the Basecamp connection
+    $config = sfConfig::get('basecamp_config', array());
+    $basecampApi = new \Sirprize\Basecamp($config);
+    
+    // grab the local table for projects
+    $projectsTable = Doctrine::getTable('project');
+    
+    // grab all the projects from the table
+    $projects = $projectManager->getAllProjectsFromDatabase($projectsTable);
+    
+    // get the time entry table, so we can write changes to it
+    $timeEntryTable = Doctrine::getTable('timeEntry');
+    
+    // go over each project
+    foreach ($projects as $project)
+    {
+      // report that we're updating entries for a certain project
+      $this->logSection('execute', sprintf('Updating time for Project: %s', $project->getName()));
+      
+      // create a manager object for this project, by id
+      $timeManager = new \basecamp2cnp\TimeEntry\Manager($basecampApi, $project->getBasecampId());
+      
+      // set the task so we get feedback over stdout
+      $timeManager->setTask($this);
+      
+      // call the update method to do the updates
+      $timeManager->updateTimeEntries($timeEntryTable);
+    }
+  }
+}
